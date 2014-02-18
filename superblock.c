f4d9d9ade4e176aa3a2a3d61aa2e7a9052e89f11
Ankit Chheda <ankit.ac.chheda@gmail.com>
2014-02-18 16:05:36 -0500

    Verifying the root as dir

--- superblock.c
+++ superblock.c
@@ -91,43 +91,9 @@ void check_inode(int inode_number)
 		printf("Not a dir\n");
 }
 
-unsigned int inode_to_sector(unsigned int inode_num)
-{
-	int parition_number = 1;
-	unsigned int num_blocks_per_group = EXT2_BLOCKS_PER_GROUP(super_block);
-	unsigned int num_sectors_per_block = EXT2_BLOCK_SIZE(super_block) / sector_size_bytes;
-	unsigned int group_number = (inode_num - 1)/EXT2_INODES_PER_GROUP(super_block);
-
-	#if 0
-	unsigned int group_start_sector = EXT2_BLOCKS_PER_GROUP(super_block) * \
-										group_number * num_sectors_per_block \
-										+ get_start_sector(parition_number);
-
-
-	
-	char group_descriptor_buf[SECTOR_SIZE];
-	read_sectors(group_descriptor_sector, 1, group_descriptor_buf);
-	struct group_descriptor_sector* group_desciptor = (struct group_descriptor_sector*) group_descriptor_buf;
-	
-	/* Reading the inode table */
-	unsigned int inode_table_sector_number =  group_desciptor->group_desc.bg_inode_table * num_sectors_per_block + group_start_sector;
-	struct ext2_inode inode_table[EXT2_INODES_PER_GROUP(super_block)];
-	int inode_table_size = (EXT2_INODES_PER_GROUP(super_block)*EXT2_INODE_SIZE(super_block)) / sector_size_bytes;
-
-
-	read_sectors(inode_table_sector_number, inode_table_size, (char*) inode_table);
-
-	int index = (inode_num - 1) % EXT2_INODES_PER_GROUP(super_block);
-	struct ext2_inode inode = inode_table[index];
-	printf("Groupid:%d\n",inode.i_gid);
-	#endif
-
-	return 1;
-}
 
 void verify_partition(int partition_start_sector)
 {
-	
 	char superblock_buf[SUPERBLOCK_SIZE];
 	int superblock_start_sector = (partition_start_sector) + SUPERBLOCK_OFFSET/SECTOR_SIZE;
 	int current_group_number = 0;
--- utils.h
+++ utils.h
@@ -35,7 +35,7 @@ struct mbr_entry
 void make_mbr();
 void print_mbr();
 void free_mbr();
-extern void verify_partition(int partition_number);
 
+extern void verify_partition(int partition_number);
 
 #endif
