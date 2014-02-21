ae0520b4843aadcceb80a77597ad2836cffe4ee7
Ankit Chheda <ankit.ac.chheda@gmail.com>
2014-02-20 23:20:36 -0500

    Correcting the superblock reading

--- superblock.c
+++ superblock.c
@@ -12,7 +12,7 @@
 #define SECTOR_SIZE sector_size_bytes
 #define SECTORS_PER_SUPERBLOCK SUPERBLOCK_OFFSET/SECTOR_SIZE
 #define MAX_INODE_ENTRIES sizeof(struct ext2_inode) * EXT2_INODES_PER_GROUP(super_block)
-#define SECTORS_PER_BLOCK EXT2_BLOCK_SIZE(super_block) / sector_size_bytes
+#define SECTORS_PER_BLOCK(m) EXT2_BLOCK_SIZE(m)/sector_size_bytes
 #define S_IFDIR  0040000
 #define S_IFMT  00170000
 #define S_ISDIR(m)      (((m) & S_IFMT) == S_IFDIR)
@@ -23,31 +23,39 @@ extern const unsigned int sector_size_bytes;
 
 static struct ext2_super_block* super_block = NULL;
 static struct ext2_group_desc* group_desc_table = NULL;
-static char* block_bitmap = NULL;
-static char* inode_bitmap = NULL;
+static char** block_bitmap_table = NULL;
+static char** inode_bitmap_table = NULL;
+static struct ext2_inode** inode_table = NULL;
 
-static struct ext2_inode* inode_table = NULL;
 static volatile int group_start_sector = 0;
 
-#if 0
-static void make_block_bitmaps(struct group_descriptor_t group_descriptor)
+static void make_block_bitmaps(int partition_start_sector, int group_number)
 {
-	int num_sectors_block_bitmap = EXT2_BLOCKS_PER_GROUP(super_block) \
-							/ (8 * SECTOR_SIZE);
+	int num_sectors_block_bitmap = CEIL(EXT2_BLOCKS_PER_GROUP(super_block), 8);
+
 	char block_buf[num_sectors_block_bitmap * sector_size_bytes];
-	int block_bitmap_sector = group_descriptor.group_desc.bg_block_bitmap * SECTORS_PER_BLOCK + group_start_sector;
 
+	printf("%d %d %d\n", group_desc_table[group_number].bg_block_bitmap, group_desc_table[group_number].bg_inode_bitmap, group_desc_table[group_number].bg_inode_table);
+	printf("BLOCKS_PER_SECTOR = %d\n", EXT2_BLOCK_SIZE(super_block));
+	
+	int block_bitmap_sector = group_desc_table[group_number].bg_block_bitmap \
+								* (EXT2_BLOCK_SIZE(super_block)/sector_size_bytes) \
+								 + partition_start_sector;
+	printf("Read Sector: %d\n",block_bitmap_sector );
 	read_sectors(block_bitmap_sector, num_sectors_block_bitmap, block_buf);
+	print_sector(block_buf);
+	int block_bitmap_size = CEIL(EXT2_BLOCKS_PER_GROUP(super_block), 8);
 
-	int block_bitmap_size = 0;
 	if((EXT2_BLOCKS_PER_GROUP(super_block) % 8) == 0)
 		block_bitmap_size = EXT2_BLOCKS_PER_GROUP(super_block) / 8;
 	else
 		block_bitmap_size = EXT2_BLOCKS_PER_GROUP(super_block) / 8 + 1;
-	memcpy(block_bitmap, block_buf, (size_t)block_bitmap_size);
+
+	//memcpy(block_bitmap_i, block_buf, (size_t)block_bitmap_size);
 }
 
 
+#if 0
 static void make_inode_bitmaps(struct group_descriptor_t group_descriptor)
 {
 
@@ -96,12 +104,15 @@ void read_super_block(int partition_start_sector)
 {
 	char superblock_buf[SUPERBLOCK_SIZE];
 	int superblock_start_sector = (partition_start_sector) + SUPERBLOCK_OFFSET/SECTOR_SIZE;
+	printf("Superblock Start sector: %d.\n", superblock_start_sector);
 	read_sectors(superblock_start_sector, SUPERBLOCK_SIZE/SECTOR_SIZE, superblock_buf);
 	super_block = calloc(1, sizeof(struct ext2_super_block));
-	super_block = (struct ext2_super_block*) superblock_buf;
+	//super_block = (struct ext2_super_block) superblock_buf;
+	memcpy(super_block, superblock_buf, sizeof(struct ext2_super_block));
+	printf("Num blocks per group : %d\n",EXT2_BLOCKS_PER_GROUP(super_block));
 }
 
-
+/* Reading the group descriptor table */
 void read_group_descriptor_table(int partition_start_sector)
 {
 	int total_number_of_groups = CEIL(super_block->s_blocks_count, EXT2_BLOCKS_PER_GROUP(super_block));
@@ -115,13 +126,19 @@ void read_group_descriptor_table(int partition_start_sector)
 		/ SECTOR_SIZE + SUPERBLOCK_SIZE / SECTOR_SIZE;
 
 	int group_desc_num_sectors = CEIL(group_desc_table_size, SECTOR_SIZE);
-#ifdef DEBUG
+
+	block_bitmap_table = calloc(total_number_of_groups, sizeof(char*));
+	inode_bitmap_table = calloc(total_number_of_groups, sizeof(char*));
+	inode_table = calloc(total_number_of_groups, sizeof(struct inode*));
+
+//#ifdef DEBUG
 	printf("Total Number of Groups = %d \n", total_number_of_groups);
 	printf("Group Desc Table Sector = %d \n", group_desc_table_sector);
 	printf("Group Desc Table Size = %d \n", 	total_number_of_groups * sizeof(struct ext2_group_desc));
 	printf("GD num_sectors = %d \n", group_desc_num_sectors);
 	printf("bufsize = %d \n", buf_size);
-#endif
+//#endif
+
 	read_sectors(group_desc_table_sector,  group_desc_num_sectors
 					,group_desc_table_buf);
 	group_desc_table =
@@ -150,6 +167,7 @@ void verify_partition(int partition_start_sector)
 
 	/* Reading Group Descriptor table */
 	read_group_descriptor_table(partition_start_sector);
+	make_block_bitmaps(partition_start_sector, 0);
 
 
 #if 0
